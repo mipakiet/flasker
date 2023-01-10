981fa5640dd3c8643cecf35218be178abb3ead67
mipakiet <mikolajpakiet@gmail.com>
2023-01-10 16:30:00 +0100

    #7

--- hello.py
+++ hello.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "dev"
+
+
+class NamerForm(FlaskForm):
+    name = StringField("What's your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 
 @app.route('/')
@@ -21,3 +30,14 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return  render_template("500.html"), 500
+
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+        flash("Form Submitted Successful")
+    return render_template("name.html", name=name, form=form)
