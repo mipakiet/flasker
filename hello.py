1103478881a9a20702a3b9ac764fdd1d8d514a3e
mipakiet <mikolajpakiet@gmail.com>
2023-01-18 20:50:45 +0100

    #10

--- hello.py
+++ hello.py
@@ -1,11 +1,52 @@
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "dev"
+app.config.from_object('config')
 
+db = SQLAlchemy()
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(200), nullable=False, unique=True)
+    data_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __ref__(self):
+        return '<Name %r>' % self.name
+
+
+class UserForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    form = UserForm()
+    name_to_update = Users.query.get_or_404(id)
+    if request.method == "POST":
+        name_to_update.name = request.form['name']
+        name_to_update.email = request.form['email']
+        try:
+            db.session.commit()
+            flash("User Updated Successfully!")
+            return render_template("update.html", form=form, name_to_update=name_to_update)
+        except:
+            flash("Error")
+            return render_template("update.html", form=form, name_to_update=name_to_update)
+    else:
+        return render_template("update.html", form=form, name_to_update=name_to_update)
 
 class NamerForm(FlaskForm):
     name = StringField("What's your name?", validators=[DataRequired()])
@@ -41,3 +82,22 @@ def name():
         form.name.data = ""
         flash("Form Submitted Successful")
     return render_template("name.html", name=name, form=form)
+
+
+@app.route('/user/add', methods=['GET', 'POST'])
+def add_user():
+    name = None
+    form = UserForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        print(user)
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+            flash("User added successfully!")
+        name = form.name.data
+        form.name.data = ''
+        form.email.data = ''
+    our_users = Users.query.order_by(Users.data_added)
+    return render_template('add_user.html', form=form, name=name, our_users=our_users)
